2e85d91acd29717b3d1f8faeea8ca39785cec880
usmc-Blockhead <jaysplaypen@gmail.com>
2022-11-28 13:04:08 -0800

    the new save

--- app/models.py
+++ app/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
 
-# Create your models here.
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -16,12 +13,3 @@ class Task(models.Model):
         return self.title
     class Meta:
         ordering = ['complete']
-
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Please provide a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-Login_URL = 'login'
+Login_URL = 'Login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
