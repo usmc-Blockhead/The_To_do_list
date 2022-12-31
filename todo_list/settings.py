b5f760a50ce1744f8fbb7c14f0dccd45fbde9600
usmc-Blockhead <jaysplaypen@gmail.com>
2022-12-30 16:35:23 -0800

    allowedhost

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-7xw#wvnvlp0@9t9mb7k_l3+iw3hr^!j6-%#%gg&79pwz_1d#ja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    squid-app-ncuno.ondigitalocean.app
+]
 
 
 # Application definition
