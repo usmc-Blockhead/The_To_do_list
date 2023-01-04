32848cdbe0cf891ffb2045cb6ee2ffd0b4a3ae73
usmc-Blockhead <jaysplaypen@gmail.com>
2023-01-03 16:53:54 -0800

    allowed hosts

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "seashell-app-uw9gu.ondigitalocean.app").split(",")
 
 
 # Application definition
